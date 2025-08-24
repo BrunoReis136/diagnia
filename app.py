5825d72507662fe62d0fa99cedfd0b3e0d7a08f7
Bruno Henrique Reis <bruhreis136@hotmail.com>
2025-08-24 19:52:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import os
 from datetime import timedelta
-from models import db, User
+from models import db, Medico
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
