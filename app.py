e126747269df63992169656e1fd98408e7d81f1b
Bruno Henrique Reis <bruhreis136@hotmail.com>
2025-08-24 15:29:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+import os
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default-secret')
