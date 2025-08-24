8b71da207eab67047c8bef1e4d36a6d879014b8e
Bruno Henrique Reis <bruhreis136@hotmail.com>
2025-08-24 19:40:45 -0300

    Update models.py

--- models.py
+++ models.py
@@ -3,23 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-# Modelo de usuário
-class User(db.Model):
-    __tablename__ = "users"  # nome da tabela no PostgreSQL
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=True)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-    
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return f"<User {self.username}>"
-
 
 class Medico(db.Model):
     __tablename__ = "medicos"
