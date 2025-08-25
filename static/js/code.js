4733f3f36dc44fb03e46d4e45625ee7bc53b16f2
Bruno Henrique Reis <bruhreis136@hotmail.com>
2025-08-25 19:14:40 -0300

    Update code.js

--- static/js/code.js
+++ static/js/code.js
@@ -1,1 +1,19 @@
+function openModal() {
+  document.getElementById("videoModal").style.display = "block";
+}
+
+function closeModal() {
+  const modal = document.getElementById("videoModal");
+  const video = document.getElementById("video");
+  modal.style.display = "none";
+  video.pause(); // pausa o vídeo ao fechar
+}
+
+// Fecha se clicar fora do conteúdo
+window.onclick = function(event) {
+  const modal = document.getElementById("videoModal");
+  if (event.target === modal) {
+    closeModal();
+  }
+}
 
